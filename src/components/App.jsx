4936329c11cd1bb212533c91361c062c5cd3380d
elfnesikogorosta <anatoliy.tymoshenko.work@gmail.com>
2023-07-21 17:13:14 +0300

    v1.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,21 @@ import Form from './Form/Form';
 import { Container, Title, SecondaryTitle } from './App.styled';
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
-
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('1');
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   updateFilter = data => {
     this.setState({
       filter: data,
